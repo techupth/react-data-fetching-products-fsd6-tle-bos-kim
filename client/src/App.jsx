60a082faa5a76b0a89d2421f71cbcde56760a335
PNKim <paljapon.nait@gmail.com>
2024-06-07 13:11:18 +0700

    deat: resolve ex-1 and ex-3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,84 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [post, setPost] = useState("load");
+  const [del, setDel] = useState("");
+
+  const dataServer = async () => {
+    try {
+      const getData = await axios.get("http://localhost:4001/products");
+      setPost(getData.data.data);
+    } catch (error) {
+      setPost("Fetching Error...");
+    }
+  };
+
+  const handleClick = async (index) => {
+    const deleteData = await axios.delete(
+      `http://localhost:4001/products/${index}`
+    );
+    setDel(deleteData);
+  };
+
+  useEffect(() => {
+    dataServer();
+  }, [del]);
+
   return (
     <div className="App">
       <div className="app-wrapper">
-        <h1 className="app-title">Products</h1>
+        <h1 className="app-title">
+          {post !== "load" ? (
+            post === "Fetching Error..." ? (
+              <div>Fetching Error...</div>
+            ) : (
+              "Products"
+            )
+          ) : (
+            ""
+          )}
+        </h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {post !== "load" ? (
+          post === "Fetching Error..." ? (
+            ""
+          ) : (
+            post.map((eachPost, index) => {
+              return (
+                <div className="product" key={index}>
+                  <div className="product-preview">
+                    <img
+                      src={eachPost.image}
+                      alt="some product"
+                      width="350"
+                      height="350"
+                    />
+                  </div>
+                  <div className="product-detail">
+                    <h1>Product name: {eachPost.name}</h1>
+                    <h2>Product price: {eachPost.price} Baht</h2>
+                    <p>Product description: {eachPost.description}</p>
+                  </div>
 
-          <button className="delete-button">x</button>
-        </div>
+                  <button
+                    className="delete-button"
+                    onClick={() => {
+                      handleClick(eachPost.id);
+                    }}
+                  >
+                    x
+                  </button>
+                </div>
+              );
+            })
+          )
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
     </div>
   );
